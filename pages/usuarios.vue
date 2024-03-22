ba3f39df4c8f55e3872f06d91b4caf697cadd2e5
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2024-03-22 17:57:40 -0600

    nueva página de usuarios

---
+++ pages/usuarios.vue
@@ -0,0 +1,9 @@
+<script setup>
+const { data } = await useFetch('/api/usuarios')
+</script>
+
+<template>
+    <div v-for="user in data">
+        {{ user.fullname }} - {{ user.name }} - {{ user._id }}
+    </div>
+</template>
