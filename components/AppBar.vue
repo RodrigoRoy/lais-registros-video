8ae65a55af265b26f767ec3e7ae20e55dfe7ea87
EderBerber <samuel_berber@ciencias.unam.mx>
2024-03-22 18:15:33 -0600

    agregar vista de unidad, actualización de AppBar

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -4,10 +4,29 @@
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </template>
         
-        <v-app-bar-title>Application Bar</v-app-bar-title>
+        <v-app-bar-title>Registros en video</v-app-bar-title>
         
-        <template v-slot:append>        
-            <v-btn icon="mdi-dots-vertical"></v-btn>
-        </template>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
+            </template>
+            
+            <v-list>
+                <v-list-item v-for="(item, i) in items" :key="i">
+                    <v-list-item-title>
+                        <v-btn class="" >
+                            {{ item.title }}
+                        </v-btn>
+                    </v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
     </v-app-bar>
 </template>
+
+<script setup>
+const  items = [
+    { title: 'Iniciar sesión' },
+    { title: 'Crear usuario' },
+]
+</script>
