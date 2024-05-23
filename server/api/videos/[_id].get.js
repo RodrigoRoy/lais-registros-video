26f22adad06b6b37a8eec9a4c3f28dd2c8b29607
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2024-05-23 17:11:30 -0600

    exportar a pdf desde el cliente/navegador

--- server/api/videos/[_id].get.js
+++ server/api/videos/[_id].get.js
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
         return await VideoSchema.findByIdAndUpdate(
                 event.context.params?._id, 
                 {$inc: { 'adicional.fetchCount': 1}}, // incremento del contador
-                {new: true}
+                {new: true, timestamps: false} // timestamps: false - evita actualizar updatedAt
             ).
             populate('controlDescripcion.documentalista', 'fullname'). // incluir el nombre de la persona documentalista
             exec()
