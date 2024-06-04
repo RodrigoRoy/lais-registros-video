7eb25bae730a7160b2dc7ca3727f4747ec5ff959
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2024-06-04 17:18:44 -0600

    footer responsivo

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,27 @@
 <template>
-    <v-footer :app="false" color="teal" height="auto" > 
+    <v-footer app color="teal" height="auto" rounded="lg"> 
         <!-- class="d-flex flex-column"  -->
-        <v-container fill-height class="text-center">
-            <v-row>
-                <v-col xs="12" sm="12" md="6">
-                    <v-container  fill-height class="text-center">
-                        <v-row align="center">
-                            <v-col >
-                                <v-img height="100" src="~/assets/Logo CONAHCYT.png"></v-img>
+        <v-container  class="text-center py-0">
+            <v-row no-gutters justify="space-around">
+                <v-col v-if="logoHeight > 20" cols="12" sm="12" md="6" class="py-2">
+                    <v-container class="text-center" >
+                        <v-row no-gutters align="center" justify="center">
+                            <v-col cols="4">
+                                <v-img :height="logoHeight" src="~/assets/Logo CONAHCYT.png"></v-img>
                             </v-col>
-                            <v-col >
-                                <v-img height="100" src="~/assets/Logo Mora.png"></v-img>
+                            <v-col cols="4">
+                                <v-img :height="logoHeight" src="~/assets/Logo Mora.png"></v-img>
                             </v-col>
-                            <v-col >
-                                <v-img height="100" src="~/assets/Logo LAIS.png"></v-img>
+                            <v-col cols="4">
+                                <v-img :height="logoHeight" src="~/assets/Logo LAIS.png"></v-img>
                             </v-col>
                         </v-row>
                     </v-container>
                 </v-col>
 
-                <v-col xs="12" sm="12" md="6">
-                    <v-container>
-                        <v-row align="end" >
+                <v-col cols="12" sm="12" md="6">
+                    <v-container class="text-center">
+                        <v-row v-if="logoHeight>20" no-gutters align="center" >
                             <v-col >
                                 <nuxt-link to="/about">
                                     <v-btn color="" variant="text">Acerca de</v-btn>
@@ -39,10 +39,14 @@
                             </v-col>
                         </v-row>
                         
-                        <v-row class="text-center">
+                        <v-row justify="space-around">
                             <div>
-                                <p xmlns:cc="http://creativecommons.org/ns#" >Este sitio está bajo una licencia <a href="https://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""></a></p>
-                                <p class="font-italic text-caption">Los contenidos son manifestaciones de libre expresión y no reflejan una postura institucional.</p> 
+                                <p class="text-caption" xmlns:cc="http://creativecommons.org/ns#" >
+                                    Este sitio está bajo una licencia <a href="https://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""></a>
+                                </p>
+                                <p class="font-italic text-caption">
+                                    Los contenidos son manifestaciones de libre expresión y no reflejan una postura institucional.
+                                </p> 
                             </div>
                         </v-row>
                     </v-container>  
@@ -53,3 +57,20 @@
 
     </v-footer>
   </template>
+<script setup>
+import { useDisplay } from 'vuetify'
+
+const { name } = useDisplay()
+
+const logoHeight = computed(() => {
+  switch (name.value) {
+    case 'xs': return 20
+    case 'sm': return 50
+    case 'md': return 80
+    case 'lg': return 100
+    case 'xl': return 100
+    case 'xxl': return 100
+  }
+  return undefined
+  })
+</script>
