e7c8ff6cebd333fdd5b7d25e4dcb3c34b420e638
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2024-06-05 17:33:12 -0600

    tema de color incorporado

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <p class="text-subtitle-1 pa-6 ma-2">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat nibh sed pulvinar proin. Ultricies integer quis auctor elit sed vulputate mi sit amet. Vel risus commodo viverra maecenas accumsan lacus vel. Massa sapien faucibus et molestie ac feugiat. Quam vulputate dignissim suspendisse in est ante in nibh. Elit eget gravida cum sociis natoque. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Sit amet volutpat consequat mauris nunc congue. Ultrices vitae auctor eu augue. Pretium fusce id velit ut tortor pretium viverra suspendisse potenti. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Purus in massa tempor nec feugiat nisl. Vitae ultricies leo integer malesuada nunc vel risus commodo viverra. Volutpat maecenas volutpat blandit aliquam etiam. Augue eget arcu dictum varius duis. Rhoncus urna neque viverra justo. Pretium viverra suspendisse potenti nullam ac tortor vitae purus.
     </p>
-    <v-btn class="ma-4">
+    <v-btn color="primary" class="ma-4">
         <nuxt-link to="/videos" class="text-decoration-none">
             Ver registros en video
         </nuxt-link>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,6 +4,7 @@ import '@mdi/font/css/materialdesignicons.css'
 // Theme configuration
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import colors from 'vuetify/util/colors'
 
 // Translations provided by Vuetify
 import { es, en } from 'vuetify/locale'
@@ -13,6 +14,27 @@ export default defineNuxtPlugin((app) => {
     // Temas de color
     theme: {
         defaultTheme: 'dark',
+        themes: {
+          light: {
+            colors:{
+              primary: '#1976D2',
+              secondary: '#424242',
+              accent: '#82B1FF',
+              error: '#FF5252',
+              info: '#2196F3',
+              success: '#4CAF50',
+              warning: '#FFC107',
+            },
+          },
+          dark: {
+            colors: {
+              primary: colors.indigo.lighten1,
+              secondary: colors.orange.darken1,
+              accent: colors.shades.black,
+              error: colors.red.accent3,
+            },
+          }
+        },
     },
     // Idiomas. Español (es) por default, inglés (en) como secundario
     locale: {
