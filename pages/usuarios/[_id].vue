935cbf7ed9abb0996dc0c4871ebaaffde52ac82d
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2024-06-13 13:57:47 -0600

    query params in api

--- pages/usuarios/[_id].vue
+++ pages/usuarios/[_id].vue
@@ -1,17 +1,10 @@
 <template>
     <v-card color="transparent">
-        <v-container>
-            <v-col cols="4">
-                <div class="text-h4 text-start">
-                    Perfil
-                </div>
-            </v-col>
-        </v-container>
-        <v-sheet class="d-flex align-center justify-center flex-wrap text-center mb-2 mx-auto px-4 bg-deep-orange-lighten-5" elevation="4"  max-width="400" width="100%" rounded >
+        <v-sheet class="d-flex align-center justify-center flex-wrap text-center mb-2 mx-auto px-4 bg-accent" elevation="4"  max-width="400" width="100%" rounded >
             <div>
                 <div class="my-2">
                     <v-avatar icon="mdi-account" :image="user.profileImage ? `/data/profile/${user.profileImage}` : '`~/assets/personalPage.gif`' " style="height: 150px; width: 150px;" variant="elevated" ></v-avatar>
-                    <p v-if="user.admin" class="mb-2 font-italic text-overline text-end">Administrador</p>
+                    <p v-if="user.admin" class="mb-2 font-italic text-overline text-center">Administrador/a</p>
                     
                 </div>
                 <div class="my-3">
@@ -43,11 +36,11 @@
     </v-card>
 
     <!-- Lista de marcadores del usuario -->
-    <v-container v-if="userBookmarks?.length > 0">
+    <v-container v-if="user.bookmarks?.length > 0">
         <v-row>
             <v-col>
                 <p>Videos guardados</p>
-                <v-data-table :headers="dataTableHeaders" :items="userBookmarks" density="compact" hide-no-data :hide-default-footer="userBookmarks?.length <= 10" hover items-per-page="5" :no-filter="true">
+                <v-data-table :headers="dataTableHeaders" :items="user.bookmarks" density="compact" hide-no-data :hide-default-footer="user.bookmarks?.length <= 10" hover items-per-page="5" :no-filter="true">
                     <!-- Portada -->
                     <template v-slot:item.portada="{ value }">
                         <v-img :src="`/data/image/${value}`" cover></v-img>
@@ -71,7 +64,7 @@
         <v-row>
             <v-col>
                 <p>Registros creados</p>
-                <v-data-table :headers="dataTableHeaders" :items="userVideosList" density="compact" hide-no-data :hide-default-footer="userVideosList?.length <= 10" hover items-per-page="5" :no-filter="true">
+                <v-data-table :headers="dataTableHeaders" :items="userVideosList" density="compact" hide-no-data :hide-default-footer="userVideosList?.length <= 5" hover items-per-page="5" :no-filter="true">
                     <!-- Portada -->
                     <template v-slot:item.portada="{ value }">
                         <v-img :src="`/data/image/${value}`" cover></v-img>
@@ -92,11 +85,11 @@
 
     <!-- Lista de videos que son borradores hechos por el usuario -->
     <!-- v-for="(borrador, i) in userDraftList" :key="userDraftList._id" -->
-    <v-container v-if="userDraftList?.length > 0">
+    <v-container v-if="user.drafts?.length > 0">
         <v-row>
             <v-col>
-                <p>Borradores *PENDIENTE - items-per-page*</p>
-                <v-data-table :headers="dataTableHeaders" :items="userDraftList" item-selectable="userDraftList.adicional.isDraft" density="compact" hide-no-data :hide-default-footer="userDraftList?.length <= 10" hover items-per-page="5" :no-filter="true">
+                <p>Borradores</p>
+                <v-data-table :headers="dataTableHeaders" :items="user.drafts" density="compact" hide-no-data :hide-default-footer="user.drafts?.length <= 10" hover items-per-page="5" :no-filter="true">
                     <!-- Portada -->
                     <template v-slot:item.portada="{ value }">
                         <v-img :src="`/data/image/${value}`" cover></v-img>
@@ -114,8 +107,6 @@
             </v-col>
         </v-row>
     </v-container>
-
-    {{ userDraftList[7].adicional.isDraft }}
 </template>
 
 <script setup>
@@ -133,23 +124,17 @@ const route = useRoute()
 const dayjs = useDayjs()
 
 // Información del usuario
-const { data: user } = await useFetch(`/api/usuarios/${route.params._id}`)
-
-// Lista de marcadores del usuario
-const { data: userBookmarks } = await useFetch(`/api/bookmarks/user/${route.params._id}`)
+const { data: user } = await useFetch(`/api/usuarios/${route.params._id}`, {query: {drafts: true, bookmarks: true}})
 
 // Lista de videos creados por el usuario
 const { data: userVideosList } = await useFetch(`/api/videos/user/${route.params._id}`)
 
-// Lista de borradores creados por el usuario
-const { data: userDraftList } = await useFetch(`/api/videos/user/${route.params._id}`)
-
 // Encabezados con los datos relevantes a mostrar para la lista de videos
 const dataTableHeaders = [
     {title: 'Portada', key: 'portada', value: 'adicional.imagen', sortable: false},
-    {title: 'Código de referencia', key: 'codigoReferencia', value: 'identificacion.codigoReferencia', sortable: false},
-    {title: 'Fecha del registro', key: 'fechaString', value: item => dayjs(item.identificacion.fecha).format('DD/MM/YYYY'), sortable: false},
-    {title: 'Duración', key: 'duracionString', value: item => minutesToHour(item.identificacion.duracion), sortable: false},
+    {title: 'Código de referencia', key: 'codigoReferencia', value: 'identificacion.codigoReferencia', sortable: true},
+    {title: 'Fecha del registro', key: 'fechaString', value: item => dayjs(item.identificacion.fecha).format('DD/MM/YYYY'), sortable: true},
+    {title: 'Duración', key: 'duracionString', value: 'identificacion.duracion', sortable: true},
     {title: 'Fecha de creación', key: 'creacionString', value: item => dayjs(item.createdAt).format('DD/MM/YYYY HH:mm'), sortable: false},
     {title: 'Acciones', key: '_id', value: '_id', sortable: false},
 ]
--- server/api/usuarios/[_id].get.js
+++ server/api/usuarios/[_id].get.js
@@ -1,11 +1,27 @@
+/**
+ * Obtener la información de un usuario
+ * @param {boolean} drafts Determina si la lista de borradores se le aplica populate()
+ * @param {boolean} bookmarks Determina si la lista de marcadores se le aplica populate()
+ */
 export default defineEventHandler(async (event) => {
     // Verificar que el _id sea una representación de 12 bytes
     const isValidId = /^[a-f0-9]{24}$/.test(event.context.params._id)
     if(!isValidId) throw createError({ statusCode: 400, statusMessage: 'ID inválido'})
 
+    // Parámetros URL: drafts, bookmarks
+    const query = getQuery(event)
+    const populateDrafts = query?.drafts && query.drafts === 'true' ? true : false
+    const populateBookmarks = query?.bookmarks && query.bookmarks === 'true' ? true : false
+
     // Buscar en base de datos
     try {
-        return await UsuarioSchema.findOne({ _id: event.context.params?._id })
+        if(populateDrafts && populateBookmarks)
+            return await UsuarioSchema.findById(event.context.params?._id).populate('drafts').populate('bookmarks').exec()
+        if(populateBookmarks)
+            return await UsuarioSchema.findById(event.context.params?._id).populate('bookmarks').exec()
+        if(populateDrafts)
+            return await UsuarioSchema.findById(event.context.params?._id).populate('drafts').exec()
+        return await UsuarioSchema.findById(event.context.params?._id)
     }
     catch (error) {
         throw createError({ statusCode: 500, statusMessage: error })
